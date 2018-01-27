452cca5fb1336aba154d89c0286fe1c737e45fde
Charlie Monk <CharlieMonk@users.noreply.github.com>
2018-01-26 20:05:49 -0500

    Updated find_cube.py to use HSV. This has improved cube detection greatly!

--- find_cube.py
+++ find_cube.py
@@ -11,11 +11,14 @@ def removeNoise(img, kernelSize):
     kernel = np.ones(kernelSize, np.uint8)
 
     # Set values for thresholding
-    cube_color_lower = np.array([0, 146, 129])
-    cube_color_upper = np.array([114, 173, 192])
+    # cube_color_lower = np.array([0, 146, 129])
+    # cube_color_upper = np.array([114, 173, 192])
+    cube_color_lower = np.array([25, 100, 100])
+    cube_color_upper = np.array([28, 255, 215])
 
     # Remove noise
     mask = cv2.inRange(img, cube_color_lower, cube_color_upper)
+    cv2.imshow("img", mask)
     close_gaps = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
     no_noise = cv2.morphologyEx(close_gaps, cv2.MORPH_OPEN, kernel)
     dilate = cv2.dilate(no_noise, np.ones((5,10), np.uint8), iterations=5)
@@ -47,17 +50,18 @@ def findObject(dilate):
             top = tuple(cnt[cnt[:,:,1].argmin()][0])
             bottom = tuple(cnt[cnt[:,:,1].argmax()][0])
 
+            print(right[0] - left[0])
             # Use boundary points to find the top left and bottom right corners
             top_left = (left[0], top[1])
             bottom_right = (right[0], bottom[1])
 
             # Draw a rectangle bounding the object using top left and bottom right points
-            cv2.rectangle(img, top_left, bottom_right, color, 3)
+            cv2.rectangle(img3, top_left, bottom_right, color, 3)
 
             # Find the center point of the object
             center_point = (int((top_left[0]+bottom_right[0])/2), int((top_left[1]+bottom_right[1])/2))
             # Draw circle at the center point
-            cv2.circle(img, center_point, 5, (0,0,255), -1)
+            cv2.circle(img3, center_point, 5, (0,0,255), -1)
             # Find the angle to the center point
             angle = getAngle(center_point)
             if(not isTesting):
@@ -71,15 +75,17 @@ def getAngle(center_point):
     field_of_view = 65
     pixel_distance = center_point[0] - width/2
     heading = ((field_of_view/2.0) * pixel_distance)/(width/2)
-    print(heading)
+    #print(heading)
     return int(heading)
 
 def sendData(angle):
     data = {
         "sender" : "vision",
-        "message" : angle
+        "message" : angle,
+        "distance" : distance
     }
     channel.send_to(json.dumps(data))
+
 
 counter = 0
 ranOnce = False
@@ -105,20 +111,21 @@ if(not isTesting):
 
 video_capture = cv2.VideoCapture(0)
 video_capture.set(cv2.CAP_PROP_FPS, 10)
-_, img = video_capture.read()
-_, width, _ = img.shape
+_, img3 = video_capture.read()
+_, width, _ = img3.shape
 # print("----"+"\n\n\nWidth: " + str(width)+"\n\n\n----")
 
 
 while(True):
     # Get the frame
-    _, img = video_capture.read()
+    _, img3 = video_capture.read()
+    img = cv2.cvtColor(img3, cv2.COLOR_BGR2HSV)
     # Enable line below if reading from precaptured image
     #img = cv2.imread("/Users/cbmonk/Downloads/testf.png")
 
     dilate = removeNoise(img, (5,5))
     img2 = findObject(dilate)
-    cv2.imshow("Objects found!", img)
+    cv2.imshow("Objects found!", img3)
 
     #Default index to use if no previous logging folders exist
     logging_folder = "0001"
@@ -128,7 +135,7 @@ while(True):
     if len(sorted_glob)>0 and (not ranOnce):
         # Make a new folder with a 4 digit name one greater than the last logging folder
         logging_folder = "{:04d}".format(int(sorted_glob[-1])+1)
-        print(logging_folder)
+        #print(logging_folder)
     if not ranOnce:
         # If this is the first time the program has been run, make a logging folder
         folder += logging_folder
